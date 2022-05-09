acf3d07d49b6d106225b2757bfc76142152ef35e
Andrew MacMurray <a.macmurray@icloud.com>
2022-05-09 19:58:20 +0100

    nice colors for logger

--- src/Elm/Debug.purs
+++ src/Elm/Debug.purs
@@ -2,10 +2,10 @@ module Elm.Debug
   ( log
   ) where
 
-import Effect.Class.Console as Console
 import Effect.Unsafe (unsafePerformEffect)
-import Elm.Basics ((++), (|>))
-import Prelude (class Show, show)
+import Elm.Basics ((|>))
+import Elm.Node.Logger as Logger
+import Prelude (class Show)
 
 log :: forall a. Show a => String -> a -> a
-log tag val = unsafePerformEffect (Console.log (tag ++ ": " ++ show val)) |> (\_ -> val)
+log tag val = unsafePerformEffect (Logger.debug tag val) |> (\_ -> val)
--- src/Elm/Node/Logger.purs
+++ src/Elm/Node/Logger.purs
@@ -11,13 +11,46 @@ import Elm.Basics ((++))
 import Prelude (class Show, Unit, show)
 
 debug :: forall m a. MonadEffect m => Show a => String -> a -> m Unit
-debug tag val = Console.log ("DEBUG - " ++ tag ++ ": " ++ show val)
+debug tag val = Console.log (message Magenta tag val)
 
 info :: forall m a. MonadEffect m => Show a => String -> a -> m Unit
-info tag val = Console.info ("INFO - " ++ tag ++ ": " ++ show val)
+info tag val = Console.info (message Cyan tag val)
 
 warn :: forall m a. MonadEffect m => Show a => String -> a -> m Unit
-warn tag val = Console.warn ("WARN - " ++ tag ++ ": " ++ show val)
+warn tag val = Console.warn (message Yellow tag val)
 
 error :: forall m a. MonadEffect m => Show a => String -> a -> m Unit
-error tag val = Console.error ("ERROR - " ++ tag ++ ": " ++ show val)
+error tag val = Console.error (message Red tag val)
+
+message :: forall a. Show a => Color -> String -> a -> String
+message color tag val =
+  colorToString color
+    ++ colorTag color
+    ++ " - "
+    ++ tag
+    ++ ": "
+    ++ show val
+    ++ reset
+
+data Color
+  = Red
+  | Yellow
+  | Cyan
+  | Magenta
+
+colorTag :: Color -> String
+colorTag = case _ of
+  Red -> "ERROR"
+  Yellow -> "WARN"
+  Cyan -> "INFO"
+  Magenta -> "DEBUG"
+
+colorToString :: Color -> String
+colorToString = case _ of
+  Red -> "\x1b[31m"
+  Yellow -> "\x1b[33m"
+  Cyan -> "\x1b[36m"
+  Magenta -> "\x1b[35m"
+
+reset :: String
+reset = "\x1b[0m"
