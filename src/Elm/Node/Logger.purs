c54162d3c4a5cf076696cd0f2837ecf10a2e5778
Andrew MacMurray <a.macmurray@icloud.com>
2022-05-09 19:33:02 +0100

    adds extra logger functions

--- src/Elm/Node/Logger.purs
+++ src/Elm/Node/Logger.purs
@@ -1,11 +1,23 @@
 module Elm.Node.Logger
   ( debug
+  , error
+  , info
+  , warn
   ) where
 
-import Effect (Effect)
+import Effect.Class (class MonadEffect)
 import Effect.Class.Console as Console
 import Elm.Basics ((++))
 import Prelude (class Show, Unit, show)
 
-debug :: forall a. Show a => String -> a -> Effect Unit
-debug tag val = Console.log (tag ++ ": " ++ show val)
+debug :: forall m a. MonadEffect m => Show a => String -> a -> m Unit
+debug tag val = Console.log ("DEBUG - " ++ tag ++ ": " ++ show val)
+
+info :: forall m a. MonadEffect m => Show a => String -> a -> m Unit
+info tag val = Console.info ("INFO - " ++ tag ++ ": " ++ show val)
+
+warn :: forall m a. MonadEffect m => Show a => String -> a -> m Unit
+warn tag val = Console.warn ("WARN - " ++ tag ++ ": " ++ show val)
+
+error :: forall m a. MonadEffect m => Show a => String -> a -> m Unit
+error tag val = Console.error ("ERROR - " ++ tag ++ ": " ++ show val)
