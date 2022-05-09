ba741f63a7855de30709d8aebf94d5cea8da73f4
Andrew MacMurray <a.macmurray@icloud.com>
2022-05-09 10:22:02 +0100

    adds readJson

--- src/Elm/Node/File.purs
+++ src/Elm/Node/File.purs
@@ -1,18 +1,23 @@
 module Elm.Node.File
   ( Path
   , read
+  , readJson
   , write
   ) where
 
 import Control.Promise (Promise)
 import Effect (Effect)
 import Elm.Basics ((>>))
+import Elm.Json as Json
 import Elm.Task (Task)
 import Elm.Task as Task
 import Prelude (Unit)
 
 type Path
   = String
+
+readJson :: forall a. Json.DecodeJson a => Path -> Task Json.Error a
+readJson = read_ >> Task.fromPromiseWith Json.parse
 
 read :: forall err. Path -> Task err String
 read = read_ >> Task.fromPromise
