33d796f9b2c5ab12d089b4c837f9b3148ace39e3
Andrew MacMurray <a.macmurray@icloud.com>
2022-05-18 10:25:22 +0100

    adds majority of prelude exports

--- src/Elm/Prelude.purs
+++ src/Elm/Prelude.purs
@@ -14,6 +14,109 @@ import Elm.Result (Result(..)) as Result
 import Elm.Task (Task) as Task
 import Elm.Array ((:)) as Array
 import Effect (Effect) as Effect
-import Prelude (class Show, class Ord, class Eq, negate, compare, class Functor, Unit, show, unit, bind, discard, pure, identity, (+), (-), (*), (/), (<=), (>=), (/=), (&&), (||), (==), (<), (>), (>>=), (=<<), (<$>), (<*>)) as Prelude
 import Elm.Basics ((<|), backPipe, (|>), forwardPipe, (++), concat, (<<), compose, (>>), forwardCompose, always, Never, never) as Basics
 import Control.Alt ((<|>)) as Alt
+import Prelude
+  ( class Applicative
+  , class Apply
+  , class Bind
+  , class BooleanAlgebra
+  , class Bounded
+  , class Category
+  , class CommutativeRing
+  , class Discard
+  , class DivisionRing
+  , class Eq
+  , class EuclideanRing
+  , class Field
+  , class Functor
+  , class HeytingAlgebra
+  , class Monad
+  , class Monoid
+  , class Ord
+  , class Ring
+  , class Semigroup
+  , class Semigroupoid
+  , class Semiring
+  , class Show
+  , type (~>)
+  , Ordering(..)
+  , Unit
+  , Void
+  , absurd
+  , add
+  , ap
+  , append
+  , apply
+  , between
+  , bind
+  , bottom
+  , clamp
+  , compare
+  , comparing
+  , conj
+  , const
+  , degree
+  , discard
+  , disj
+  , div
+  , eq
+  , flap
+  , flip
+  , gcd
+  , identity
+  , ifM
+  , join
+  , lcm
+  , liftA1
+  , liftM1
+  , map
+  , max
+  , mempty
+  , min
+  , mod
+  , mul
+  , negate
+  , not
+  , notEq
+  , one
+  , otherwise
+  , pure
+  , recip
+  , show
+  , sub
+  , top
+  , unit
+  , unless
+  , unlessM
+  , void
+  , when
+  , whenM
+  , zero
+  , (#)
+  , ($)
+  , ($>)
+  , (&&)
+  , (*)
+  , (*>)
+  , (+)
+  , (-)
+  , (/)
+  , (/=)
+  , (<)
+  , (<#>)
+  , (<$)
+  , (<$>)
+  , (<*)
+  , (<*>)
+  , (<=)
+  , (<>)
+  , (<@>)
+  , (=<<)
+  , (==)
+  , (>)
+  , (>=)
+  , (>=>)
+  , (>>=)
+  , (||)
+  )
